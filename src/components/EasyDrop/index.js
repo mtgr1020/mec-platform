b3ce91200bc16d842c797b59109c66846f6199cc
mtgr <mtgr1020@gmail.com>
2020-11-06 17:57:23 +0800

    完善拖拽

--- src/components/EasyDrop/index.js
+++ src/components/EasyDrop/index.js
@@ -1,31 +1,50 @@
-import { defineComponent, h, inject, ref } from "@vue/runtime-core";
+import { defineComponent, h, inject, onUpdated, ref } from "@vue/runtime-core";
 export default defineComponent({
   name: "EasyDrop",
+  props: {
+    subClass: String,
+  },
   setup() {
     // 注入拖拽节点的获取
     const slotEls = ref([])
-
     const dragEl = inject('dragEl')
+
     const handleDropEvent = (event) => {
       event.preventDefault() // 避免浏览器对数据的默认处理
-      slotEls.value.push(dragEl)
+      slotEls.value.push(dragEl[0].default) // 如果直接获取dragEl会导致全员地址指向唯一
     };
 
     const handleDargOverEvent = (event) => {
       event.preventDefault() // 必须设置此属性,才可以触发Drop 默认无法放置到其他元素
     }
 
+    const handleDragElMove = (index) => {
+      slotEls.value.splice(index, 1)
+    }
+
+    onUpdated(() => {
+      const props = dragEl[0].default()[0].props
+      if (props && props.renderRely) {
+        props.renderRely(props.ref)
+      }
+
+    })
+
     return {
+      slotEls,
       handleDropEvent,
       handleDargOverEvent,
-      slotEls
+      handleDragElMove
+
     };
   },
   render(ctx) {
     const createSlotComponent = () => {
-      return ctx.slotEls.map(slotEl => {
-        console.log(slotEl)
-        return h("div", {}, slotEl[0].default())
+      return ctx.slotEls.map((slotDefault, index) => {
+        return h("div", {
+          class: ctx.subClass,
+          onDblClick: () => { ctx.handleDragElMove(index) }
+        }, slotDefault())
       })
     }
 
--- src/pages/DragDemo/index.vue
+++ src/pages/DragDemo/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="drag-container">
-    <EasyDrop class="drop-wrap"></EasyDrop>
-    <div class="drag-list-wrap">
+    <EasyDrop class="drop-wrap" :subClass="'drop-sub'"></EasyDrop>
+    <div :class="['drag-list-wrap', isPackUp ? 'list-pack-up' : '']">
       <EasyDrag class="drag-list-item" v-for="item in dragList" :key="item">
         <div>
           {{ item }}
@@ -14,6 +14,17 @@
           <button @click="independent++">add</button>
         </div>
       </EasyDrag>
+      <EasyDrag class="drag-list-item">
+        <div
+          style="width: 300px; height: 300px"
+          ref="chart"
+          :renderRely="initMockCharts"
+        ></div>
+      </EasyDrag>
+      <div
+        :class="['drap-dent-common', isPackUp ? 'outdent' : 'indent']"
+        @click="isPackUp = !isPackUp"
+      ></div>
     </div>
   </div>
 </template>
@@ -21,13 +32,25 @@
 <script>
 import EasyDrag from "../../components/EasyDrag";
 import EasyDrop from "../../components/EasyDrop";
+import echarts from "echarts/lib/echarts";
+import "echarts/lib/chart/bar";
+import "echarts/lib/component/tooltip";
+import "echarts/lib/component/title";
+import { mockBarData } from "./mockChartOpt";
+
+// 引入柱状图
 export default {
   data() {
     return {
       dragList: [1, 2],
       dargElement: [],
       independent: 0,
+      isPackUp: false, // 是否收起列表栏
     };
+  },
+
+  mounted() {
+    this.initMockCharts("chart");
   },
 
   provide() {
@@ -40,6 +63,9 @@ export default {
     handleColletDargElement(slot) {
       this.dargElement.splice(0, 1, slot);
     },
+    initMockCharts(key) {
+      echarts.init(this.$refs[key]).setOption(mockBarData);
+    },
   },
   components: {
     EasyDrag,
@@ -48,23 +74,62 @@ export default {
 };
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
+@space: 8px;
+@drag-bg-color: rgba(24, 51, 92, 0.548);
 .drag-container {
   height: 100%;
   display: flex;
   .drop-wrap {
     flex-grow: 1;
-  }
-  .drag-list-wrap {
-    flex-basis: 200px;
     display: flex;
     flex-wrap: wrap;
+    padding: @space;
+    .drop-sub {
+      flex-basis: 33.33%;
+      background-color: @drag-bg-color;
+      max-height: 300px;
+      margin: 0 5px;
+    }
+  }
+
+  @basis-width: 200px;
+  .drag-list-wrap {
+    @backcolor: rgb(95, 204, 153);
+    @min-height: 100px;
+
+    flex-basis: @basis-width;
+    display: flex;
+    flex-wrap: wrap;
+    position: relative;
+    padding: @space;
+    background-color: @backcolor;
     .drag-list-item {
       width: 100%;
-      min-height: 100px;
-      background-color: palevioletred;
-      margin-bottom: 10px;
+      min-height: @min-height;
+      background-color: @drag-bg-color;
+      margin-bottom: @space;
     }
+    .drap-dent-common {
+      @dent-size: 30px;
+      position: absolute;
+      left: -@dent-size;
+      width: @dent-size;
+      height: @dent-size;
+      background-color: @backcolor;
+
+      background-size: 100% 100%;
+    }
+    .indent {
+      background-image: url("../../assets/img/indent.png");
+    }
+
+    .outdent {
+      background-image: url("../../assets/img/outdent.png");
+    }
+  }
+  .list-pack-up {
+    transform: translateX(@basis-width);
   }
 }
 </style>
